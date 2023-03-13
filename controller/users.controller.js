094115203747900702823e56807c4af3ff5e7049
itzshakeeb <itzshakeeb@gmail.com>
2023-03-13 14:42:19 +0600

    added update route

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,10 +1,13 @@
 const { getDb } = require("../utilities/mongodb")
-
+const { ObjectId } = require("mongodb")
 // Get all users
 module.exports.getUsers = async (req, res, next) => {
     try {
         const db = getDb()
         const users = await db.collection("users").find().toArray()
+        if (!users) {
+            return res.status(400).json({ success: false, data: `no user found` })
+        }
         res.status(200).json({ success: true, data: users })
     }
     catch (error) {
@@ -17,13 +20,24 @@ module.exports.postUser = async (req, res, next) => {
     try {
         const db = getDb()
         const user = await db.collection("users").insertOne(req.body)
-        console.log(user);
+        // console.log(user);
         if (!user.insertedId) {
             return res.status(400).json({ success: false, data: `something is Wrong` })
         }
         res.status(200).json({ success: true, data: `user is created id : ${user.insertedId}` })
     }
-    catch (error) {
-        next(error)
-    }
+    catch (error) { next(error) }
+}
+//update a user
+module.exports.updateUser = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const db = getDb()
+        const user = await db.collection("users").updateOne({ _id: ObjectId(id) }, { $set: req.body })
+        console.log(user);
+        if (!user.modifiedCount) {
+            return res.status(400).json({ success: false, data: `update is not successful` })
+        }
+        res.status(200).json({ success: true, data: `user is updated id : ${id}` })
+    } catch (error) { next(error) }
 }
--- routes/v1/users.route.js
+++ routes/v1/users.route.js
@@ -22,4 +22,16 @@ router.get('/', usersApi.getUsers);
 **/
 router.post('/', usersApi.postUser);
 
+/**
+ * PUT /api/v1/data/:id
+ * @summary Update a data
+ * @tags data
+ * @desc Update a data
+ * @access  Public
+ * @return  the user id
+ * @param {string} id.path.required - id of the data
+ * 
+ **/
+router.put('/:id', usersApi.updateUser);
+
 module.exports = router;
